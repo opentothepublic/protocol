303578ff90230860f4d8212fdfcd46ef34bf2331
ronykris <rony.kris@gmail.com>
2024-03-09 00:36:01 +0530

    update

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,2 @@
 export const NEXT_PUBLIC_PORT = process.env.PORT || '4001'
-export const NEXT_PUBLIC_URL = `http://localhost:${NEXT_PUBLIC_PORT}`
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://ottp-attest-client.vercel.app/'
