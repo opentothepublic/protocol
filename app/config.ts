0aed45cc84d4d17d4a2bf2af72174f3cd73c0748
ronykris <rony.kris@gmail.com>
2024-03-12 00:11:07 +0530

    txn frame

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
 export const NEXT_PUBLIC_PORT = process.env.PORT || '4001'
-//export const NEXT_PUBLIC_URL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}` : 'https://ottp-attest-client.vercel.app'
-export const NEXT_PUBLIC_URL = 'https://bc0a-49-207-213-37.ngrok-free.app'
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}` : 'https://ottp-attest-client.vercel.app'
+//export const NEXT_PUBLIC_URL = 'https://bc0a-49-207-213-37.ngrok-free.app'
