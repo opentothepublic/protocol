77a60d6c6a30cf984e3da2bfa7fdc5bdf921d7c3
ronykris <rony.kris@gmail.com>
2024-03-20 02:59:34 +0530

    update

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -138,4 +138,29 @@ const getNewAttestId = async (txnId: string): Promise<any> => {
     }
 }
 
+const cast = async () => {
+    const options = {
+        method: 'POST',
+        url: 'https://api.neynar.com/v2/farcaster/cast',
+        headers: {
+          accept: 'application/json',
+          api_key: process.env.NEYNAR_API_KEY,
+          'content-type': 'application/json'
+        },
+        data: {
+          signer_uuid: process.env.SIGNER_UUID,
+          text: 'GM! Watchout this space for @ottp; the BASED collaboration protocol'
+        }
+      };
+      
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+}
+
 export {onchainAttestation, getFids, validateCollabUserInput, getTaggedData, getNewAttestId}
